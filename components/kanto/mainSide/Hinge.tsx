0f1a82abded27d8c997c6acfb0007b890ce4960f
Vic <v.chaves@betterplaceapp.com>
2023-01-19 18:30:27 +0100

    Add hinge distribution

---
+++ components/kanto/mainSide/Hinge.tsx
@@ -0,0 +1,11 @@
+import styles from "../../../styles/components/kanto/mainSide/Hinge.module.css";
+
+const Hinge = () => (
+  <div className={styles.hingeContainer}>
+    <div className={styles.hingeSmall}></div>
+    <div className={styles.hingeBig}></div>
+    <div className={styles.hingeSmall}></div>
+  </div>
+);
+
+export default Hinge;
