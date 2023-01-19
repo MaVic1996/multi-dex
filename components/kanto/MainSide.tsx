0aca04f11637ad44de89a739504ecc7d95b602fa
Vic <v.chaves@betterplaceapp.com>
2023-01-19 18:30:08 +0100

    Fix top side distribution

--- components/kanto/MainSide.tsx
+++ components/kanto/MainSide.tsx
@@ -1,9 +1,11 @@
 import styles from "../../styles/components/kanto/MainSide.module.css";
+import Hinge from "./mainSide/Hinge";
 import TopSide from "./mainSide/TopSide";
 
 const MainSide = () => (
   <div className={styles.mainSide}>
-    <TopSide/>
+    <TopSide />
+    <Hinge />
   </div>
 );
 
