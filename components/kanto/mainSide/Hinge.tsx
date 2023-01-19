7cbd683cce6bccfe6d6fba9bbab959a4d23c7be0
Víctor M. Chaves García <victormcg96@gmail.com>
2023-01-20 00:13:46 +0100

    Finish hinge component

--- components/kanto/mainSide/Hinge.tsx
+++ components/kanto/mainSide/Hinge.tsx
@@ -3,7 +3,9 @@ import styles from "../../../styles/components/kanto/mainSide/Hinge.module.css";
 const Hinge = () => (
   <div className={styles.hingeContainer}>
     <div className={styles.hingeSmall}></div>
+    <div className={styles.splitter}></div>
     <div className={styles.hingeBig}></div>
+    <div className={styles.splitter}></div>
     <div className={styles.hingeSmall}></div>
   </div>
 );
