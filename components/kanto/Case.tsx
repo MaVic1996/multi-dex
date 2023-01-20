ab71820f0e9418c7a6dbbd00add305a972b91b7f
Víctor M. Chaves García <victormcg96@gmail.com>
2023-01-20 14:46:44 +0100

    Add border details in case

--- components/kanto/Case.tsx
+++ components/kanto/Case.tsx
@@ -1,5 +1,13 @@
 import styles from "../../styles/components/kanto/Case.module.css";
 
-const Case = () => <div className={styles.case}></div>;
+const Case = () => (
+  <div className={styles.case}>
+    <div className={styles.topVoid}>
+      <div className={styles.voidTriangle} />
+      <div className={styles.diagonalBorder} />
+      <div className={styles.voidRectangle} />
+    </div>
+  </div>
+);
 
 export default Case;
